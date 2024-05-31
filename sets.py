d21156cb166e5cd53bcd73bb1bf384a1af3c80ed
lucasmolina4 <162722811+lucasmolina4@users.noreply.github.com>
2024-05-30 22:53:26 -0300

    Lucas Molinasets.py

--- sets.py
+++ sets.py
@@ -2,30 +2,15 @@ from sets_categories_data import (ALCOHOLS)
 
 
 def clean_ingredients(dish_name, dish_ingredients):
-    """Remove duplicates from `dish_ingredients`.
-
-    :param dish_name: str - containing the dish name.
-    :param dish_ingredients: list - dish ingredients.
-    :return: tuple - containing (dish_name, ingredient set).
-
-    This function should return a `tuple` with the name of the dish as the first item,
-    followed by the de-duped `set` of ingredients as the second item.
-    """
-
-    return ()
+    unique_ingredients = set(dish_ingredients)
+    return (dish_name, unique_ingredients)
+print(clean_ingredients('Punjabi-Style Chole', ['onions', 'tomatoes', 'ginger paste', 'garlic paste', 'ginger paste', 'vegetable oil', 'bay leaves', 'cloves', 'cardamom', 'cilantro', 'peppercorns', 'cumin powder', 'chickpeas', 'coriander powder', 'red chili powder', 'ground turmeric', 'garam masala', 'chickpeas', 'ginger', 'cilantro']))
 
 
 def check_drinks(drink_name, drink_ingredients):
-    """Append "Cocktail" (alcohol)  or "Mocktail" (no alcohol) to `drink_name`, based on `drink_ingredients`.
-
-    :param drink_name: str - name of the drink.
-    :param drink_ingredients: list - ingredients in the drink.
-    :return: str - drink_name appended with "Mocktail" or "Cocktail".
-
-    The function should return the name of the drink followed by "Mocktail" (non-alcoholic) and drink
-    name followed by "Cocktail" (includes alcohol).
-
-    """
-
-    return ""
-
+    drink_ingredients_set = set(drink_ingredients)
+    if drink_ingredients_set & ALCOHOLS:
+        return f"{drink_name} Cocktail"
+    else:
+        return f"{drink_name} Mocktail"
+print(check_drinks('Honeydew Cucumber', ['honeydew', 'coconut water', 'mint leaves', 'lime juice', 'salt', 'english cucumber']))
